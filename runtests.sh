460ddc231c161e993f0517642553012f426c4d4d
Krzysztof Charlikowski <kryzyyspl@gmail.com>
2020-06-09 20:50:56 +0200

    Change runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -5,3 +5,6 @@ do
     echo $i >> $1
     python main.py $2/$i >> $1
 done
+# uncomment to print to file $3 in format:
+# fileName sequenceLength \n
+# cat $1 | awk '$1 ~ /^[0-9]+$/ {print nazwa_pliku, $3} $1 ~ /txt$/ {nazwa_pliku = $1}' > $3
